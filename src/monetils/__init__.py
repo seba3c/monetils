5db4f4b64e838cf2d994e8e08b7c769de225e2ed
Sebastian Castañeda <castanedacs@gmail.com>
2026-09-06 15:49:35 +0200

    Fixed version number

--- src/monetils/__init__.py
+++ src/monetils/__init__.py
@@ -2,6 +2,6 @@ from monetils._btc import BTC
 from monetils._errors import CurrencyMismatchError, MonetilsError
 from monetils._usd import USD
 
-__version__ = "0.2.0b1"
+__version__ = "0.2.0b2"
 
 __all__: list[str] = ["BTC", "USD", "CurrencyMismatchError", "MonetilsError"]
